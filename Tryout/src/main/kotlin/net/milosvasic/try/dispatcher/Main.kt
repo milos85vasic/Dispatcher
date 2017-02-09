858ad903e089fbeeb3d68d54896c6dd7fc87c5f2
Milos Vasic <office@milosvasic.net>
2017-02-09 13:09:15 +0100

    Dispatcher: First time try.

--- Tryout/src/main/kotlin/net/milosvasic/try/dispatcher/Main.kt
+++ Tryout/src/main/kotlin/net/milosvasic/try/dispatcher/Main.kt
@@ -1,7 +1,45 @@
 package net.milosvasic.`try`.dispatcher
 
+import net.milosvasic.dispatcher.Dispatcher
+import net.milosvasic.dispatcher.routing.DynamicRouteElement
+import net.milosvasic.dispatcher.routing.Route
+import net.milosvasic.dispatcher.routing.StaticRouteElement
+import net.milosvasic.logger.ConsoleLogger
+
+private class TryDispatcher
+
 fun main(args: Array<String>) {
 
+    val LOG_TAG = TryDispatcher::class
+    val logger = ConsoleLogger()
 
+    // TODO: We should implement handling / entry point
+    val root = Route.Builder().addRouteElement(StaticRouteElement("")).build()
+
+    val routeUserRepos = Route.Builder()
+            .addRouteElement(StaticRouteElement("users"))
+            .addRouteElement(DynamicRouteElement("username"))
+            .addRouteElement(StaticRouteElement("repositories"))
+            .build()
+
+    val routeAllRepos = Route.Builder()
+            .addRouteElement(StaticRouteElement("repositories"))
+            .build()
+
+    val routeAllUsers = Route.Builder()
+            .addRouteElement(StaticRouteElement("users"))
+            .build()
+
+    val dispatcher = Dispatcher()
+    dispatcher.addRoute(routeUserRepos)
+    dispatcher.addRoute(routeAllRepos)
+    dispatcher.addRoute(routeAllUsers)
+    dispatcher.addRoute(root)
+
+    try {
+        dispatcher.start(2507)
+    } catch (e: Exception) {
+        logger.e(LOG_TAG, "Error: " + e)
+    }
 
 }
